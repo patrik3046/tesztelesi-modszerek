bf22463bbe03f12ea01c735bb05f764a33234d91
Kószó Patrick <k.patrik00@gmail.com>
2024-05-12 13:19:44 +0200

    BinomialCoefficient.java branch coverage test case

--- src/test/java/com/thealgorithms/maths/BinomialCoefficientTest.java
+++ src/test/java/com/thealgorithms/maths/BinomialCoefficientTest.java
@@ -13,4 +13,11 @@ public class BinomialCoefficientTest {
         assertEquals(84, BinomialCoefficient.binomialCoefficient(9, 3));
         assertEquals(1, BinomialCoefficient.binomialCoefficient(17, 17));
     }
+
+    @Test
+    void testBinomialCoefficient2() {
+        assertEquals(0, BinomialCoefficient.binomialCoefficient(20, 21));
+        assertEquals(0, BinomialCoefficient.binomialCoefficient(12, 51));
+        assertEquals(0, BinomialCoefficient.binomialCoefficient(9, 31));
+    }
 }
